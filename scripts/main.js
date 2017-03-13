8273d6b7fca3434b9d8bdb2272fcf6b3aee9347a
Ani Sinanaj <ani.sinanaj@caffeina.com>
2017-03-13 17:55:36 +0100

    Updated main.js for the project initializer

--- scripts/main.js
+++ scripts/main.js
@@ -32,59 +32,19 @@ function gitCommit(message) {
 	shell.exec('git add -A && git commit -am "' + message + '" && git push');
 }
 
+function createProject(name) {
+	shell.exec('mkdir -p name');
+}
+
 ////////////
-// Assets //
+// Init P //
 ////////////
 
 app.cli
-.command('assets')
-.description('Generate assets')
+.command('init')
+.description('Initialize project')
 .action(function() {
 	var self = this;
 
-	reqTiProjectOrExit();
-
-	var ticons = require('ticons');
-
-	app.out.step("Removing low resolution directories...");
-	try {
-		app.fs.deleteDirSync('app/assets/android/images');
-		app.fs.deleteDirSync('app/assets/images');
-		app.out.writeln("OK");
-	} catch (err) {}
-
-	app.out.step("Generating assets...");
-	ticons.assets({}, function(err, status) {
-		if (err) {
-			app.out.exit(err);
-			return;
-		}
-		app.out.writeln("OK");
-	});
-});
-
-/////////////////////////////
-// Install developer build //
-/////////////////////////////
-
-app.cli
-.command('devinstall')
-.option('-p, --platform <platform>', 'Platforms to build for', /^(ios|android|all)$/i, 'all')
-.description('Install current developer build on the device')
-.action(function() {
-	var self = this;
-
-	reqTiProjectOrExit();
-
-	var tiapp = require('tiapp.xml').load();
-
-	if (this.platform === 'ios' || this.platform === 'all') {
-		app.out.step("Installing developer build <b>" + tiapp.name + " v" + tiapp.version + "</b> for <b>iOS</b>...");
-		shell.exec('ideviceinstaller -i "./build/iphone/build/Products/Debug-iphoneos/' + tiapp.name + '.ipa"');
-	}
-
-	if (this.platform === 'android' || this.platform === 'all') {
-		app.out.step("Installing developer build <b>" + tiapp.name + " v" + tiapp.version + "</b> for <b>Android</b>...");
-		shell.exec('adb install "./build/android/bin/' + tiapp.name + '.apk"');
-	}
+	app.out.step("Removing low resolution directories...", JSON.stringify(self));
 });
